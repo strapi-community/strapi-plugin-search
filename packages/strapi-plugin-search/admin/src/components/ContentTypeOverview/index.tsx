16802a24131bd413ca43c0f5d2089c280d60678e
Devamsh Manoj <70159114+itsnoa04@users.noreply.github.com>
2023-08-09 23:56:04 +0530

    feat(ui): search overview

---
+++ packages/strapi-plugin-search/admin/src/components/ContentTypeOverview/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button } from "@strapi/design-system";
+import StyledTable from "../StyledTable";
+
+const action = <Button variant="secondary">Re-Index</Button>;
+
+const CTOverview = () => {
+  return (
+    <StyledTable
+      data={[
+        {
+          "Content Type": "Blog",
+          Index: "blog",
+          Engine: "Elasticsearch",
+          Documents: 15,
+        },
+        {
+          "Content Type": "Page",
+          Index: "page",
+          Engine: "Elasticsearch",
+          Documents: 2,
+        },
+        {
+          "Content Type": "Post",
+          Index: "post",
+          Engine: "Elasticsearch",
+          Documents: 15,
+        },
+        {
+          "Content Type": "Product",
+          Index: "product",
+          Engine: "Elasticsearch",
+          Documents: 2,
+        },
+      ]}
+      action={action}
+    />
+  );
+};
+
+export default CTOverview;
--- packages/strapi-plugin-search/admin/src/pages/App/index.tsx
+++ packages/strapi-plugin-search/admin/src/pages/App/index.tsx
@@ -5,17 +5,15 @@
  *
  */
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { AnErrorOccurred } from '@strapi/helper-plugin';
-import pluginId from '../../pluginId';
-import HomePage from '../HomePage';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { AnErrorOccurred } from "@strapi/helper-plugin";
+import pluginId from "../../pluginId";
 
 const App = () => {
   return (
     <div>
       <Switch>
-        <Route path={`/plugins/${pluginId}`} component={HomePage} exact />
         <Route component={AnErrorOccurred} />
       </Switch>
     </div>
