f902cb3d66476d5bb7588d5b188720fd4c904c28
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-08-09 20:51:18 -0400

    feat(server): add lifecycles

--- packages/strapi-plugin-search/server/register.ts
+++ packages/strapi-plugin-search/server/register.ts
@@ -1,5 +1,29 @@
-import { Strapi } from '@strapi/strapi';
+import type { Strapi } from "@strapi/strapi";
+import { getConfig } from "./utils";
+import type { ContentType } from "./types";
 
 export default ({ strapi }: { strapi: Strapi }) => {
-  // registeration phase
+  // register a document id field for all indexed content types
+  const indexedContentTypes = getConfig<ContentType[]>({
+    strapi,
+    path: "contentTypes",
+    defaultValue: [],
+  });
+
+  const documentIdField = getConfig<string>({
+    strapi,
+    path: "global.documentIdField",
+  });
+
+  for (const indexedCT of indexedContentTypes) {
+    if (
+      strapi.contentTypes[indexedCT.uid] &&
+      !strapi.contentTypes[indexedCT.uid].attributes[documentIdField]
+    ) {
+      strapi.log.info(`injected ${documentIdField} into ${indexedCT.uid}`);
+      strapi.contentTypes[indexedCT.uid].attributes[documentIdField] = {
+        type: "string",
+      };
+    }
+  }
 };
