6ca07cfa731bae11a037d4e02b3ada74f84f4a9b
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-10-16 23:38:05 -0400

    chore: format

--- packages/strapi-plugin-search/server/config/schema.ts
+++ packages/strapi-plugin-search/server/config/schema.ts
@@ -1,42 +1,41 @@
 import { z } from "zod";
 
 const Field = z.object({
-  name:  z.string(),
-  alias:  z.string().optional(),
-  custom:z.boolean().optional(),
-  value: z.union([z.string(),z.function()]).optional(),
+	name: z.string(),
+	alias: z.string().optional(),
+	custom: z.boolean().optional(),
+	value: z.union([z.string(), z.function()]).optional(),
 });
 
 const ContentTypeIndex = z.object({
-  name: z.string(),
-  prefix: z.string().optional(),
-  engine:z.string().optional(),
+	name: z.string(),
+	prefix: z.string().optional(),
+	engine: z.string().optional(),
 	fields: z.array(Field).optional(),
-})
+});
 
 const ContentType = z.object({
-  uid: z.string(),
+	uid: z.string(),
 	enabled: z.boolean().optional(),
-  indexes: z.array(ContentTypeIndex),
+	indexes: z.array(ContentTypeIndex),
 });
 
 const plugin = z.object({
 	global: z.object({
-    index: z.string(),
-    engine: z.string(),
-    fields : z.array(z.union([
-      z.string(),
-      Field,
-    ])).optional()
-  }),
-	engines: z.array(
-    z.object({
-      name:z.string(),
-      resolve: z.string().optional(),
-      enabled: z.boolean().optional(),
-      options: z.record(z.unknown()),
-    })
-  ).optional(),
+		index: z.string(),
+		engine: z.string(),
+		fields: z.array(z.union([z.string(), Field])).optional(),
+	}),
+	engines: z
+		.array(
+			z.object({
+				name: z.string(),
+				resolve: z.string().optional(),
+				enabled: z.boolean().optional(),
+				options: z.record(z.unknown()),
+			})
+		)
+		.optional(),
 	contentTypes: z.array(ContentType),
 });
 
